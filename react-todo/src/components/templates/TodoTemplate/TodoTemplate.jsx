59b2a5f7083a3f39b2357b001cd7352a2ea153c2
西村健太 <>
2024-05-20 13:23:58 +0900

    feature/react-todo TODO追加機能を実装

--- react-todo/src/components/templates/TodoTemplate/TodoTemplate.jsx
+++ react-todo/src/components/templates/TodoTemplate/TodoTemplate.jsx
@@ -1,12 +1,15 @@
 import { useState,useMemo } from "react";
 import { InputForm } from "../../atoms/InputForm/InputForm";
 import { TodoList } from "../../organisms/TodoLlist/TodoList";
-import { INIT_TODO_LIST } from "../../../constants/data";
+import { AddTodo } from "../../organisms/AddTodo/AddTodo";
+import { INIT_TODO_LIST,INIT_UNIQUE_ID } from "../../../constants/data";
 import styles from "./styles.module.css";
 
 export const TodoTemplate = () => {
-  const [originTodoList] = useState(INIT_TODO_LIST);
+  const [originTodoList, setOriginTodoList] = useState(INIT_TODO_LIST);
   const [searchKeyword, setSearchKeyword] = useState("");
+  const [addInputValue, setAddInputValue] = useState("");
+  const [uniqueId, setUniqueId] = useState(INIT_UNIQUE_ID);
 
   const showTodoList = useMemo(() => {
     return originTodoList.filter((todo) => {
@@ -15,11 +18,39 @@ export const TodoTemplate = () => {
     });
   }, [originTodoList, searchKeyword]);
 
+  const onChangeAddInputValue = (e) => setAddInputValue(e.target.value);
+
+  const handleAddTodo = (e) => {
+    if (e.key === "Enter" && addInputValue !== "") {
+      const nextUniqueId = uniqueId + 1;
+      const newTodoList = [
+        ...originTodoList,
+        {
+          id: nextUniqueId,
+          title: addInputValue,
+        },
+      ];
+      setOriginTodoList(newTodoList);
+
+      setUniqueId(nextUniqueId);
+
+      setAddInputValue("");
+    }
+  };
+
   const handleChangeSearchKeyword = (e) => setSearchKeyword(e.target.value);
 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Todo List</h1>
+
+      <section className={styles.common}>
+        <AddTodo
+          addInputValue={addInputValue}
+          onChangeTodo={onChangeAddInputValue}
+          handleAddTodo={handleAddTodo}
+        />
+      </section>
 
       <section className={styles.common}>
         <InputForm
