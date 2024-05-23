3671a29aea4ab3a64c3c40a6d02290455c963e50
西村健太 <>
2024-05-23 15:02:22 +0900

    feature/react-todo-context-api 削除しすぎた記述を修正

--- react-todo/src/contexts/TodoContext.jsx
+++ react-todo/src/contexts/TodoContext.jsx
@@ -1,14 +1,33 @@
 import React, { createContext, useContext } from 'react';
-import useTodos from '../hooks/useTodos';
 
 const TodosContext = createContext();
 
 export const TodosProvider = ({ children }) => {
-  const todos = useTodos();
+  const {
+    addInputValue,
+    handleAddTodo,
+    handleChangeSearchKeyword,
+    handleDeleteTodo,
+    onChangeAddInputValue,
+    searchKeyword,
+    showTodoList,
+  } = useTodos();
   return (
-    <TodosContext.Provider value={todos}>{children}</TodosContext.Provider>
+    <TodosContext.Provider
+      value={{
+        addInputValue,
+        handleAddTodo,
+        handleChangeSearchKeyword,
+        handleDeleteTodo,
+        onChangeAddInputValue,
+        searchKeyword,
+        showTodoList,
+      }}
+    >
+      {children}
+    </TodosContext.Provider>
   );
 };
 
 // eslint-disable-next-line react-refresh/only-export-components
-export const useTodosContext = () => useContext(TodosContext);
+export const useTodos = () => useContext(TodosContext);
