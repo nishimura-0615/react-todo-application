0181cf954582f87d54f613ca47f693323d23d5c9
西村健太 <>
2024-05-28 11:38:07 +0900

    feature/react-route-api Todo編集画面と編集機能を実装

---
+++ react-todo/src/components/templates/TodoEditTemplate/index.jsx
@@ -0,0 +1,96 @@
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useTodosContext } from '../../../contexts/TodoContext';
+import { BaseLayout } from '../../organisms/BaseLayout';
+import { InputForm } from '../../atoms/InputForm';
+import { TextArea } from '../../atoms/TextArea';
+import { CommonButton } from '../../atoms/CommonButton';
+import styles from './styles.module.css';
+
+const useTodoEditTemplate = ({ originTodoList, updateTodo }) => {
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const todo = useMemo(
+    () => originTodoList.find((todo) => String(todo.id) === id),
+    [id, originTodoList],
+  );
+
+  const [inputTitle, setInputTitle] = useState(todo?.title || '');
+  const [inputContent, setInputContent] = useState(todo?.content || '');
+
+  useEffect(() => {
+    if (todo) {
+      setInputTitle(todo.title);
+      setInputContent(todo.content);
+    }
+  }, [todo]);
+
+  const handleChangeTitle = useCallback(
+    (e) => setInputTitle(e.target.value),
+    [],
+  );
+
+  const handleChangeContent = useCallback(
+    (e) => setInputContent(e.target.value),
+    [],
+  );
+
+  const handleUpdateTodo = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (todo?.id && inputTitle !== '' && inputContent !== '') {
+        updateTodo(todo.id, inputTitle, inputContent);
+        navigate('/');
+      }
+    },
+    [navigate, todo?.id, inputTitle, inputContent, updateTodo],
+  );
+
+  return {
+    todo,
+    inputTitle,
+    inputContent,
+    handleChangeTitle,
+    handleChangeContent,
+    handleUpdateTodo,
+  };
+};
+
+export const TodoEditTemplate = () => {
+  const { originTodoList, updateTodo } = useTodosContext();
+
+  const {
+    todo,
+    inputTitle,
+    inputContent,
+    handleChangeTitle,
+    handleChangeContent,
+    handleUpdateTodo,
+  } = useTodoEditTemplate({ originTodoList, updateTodo });
+
+  return (
+    <BaseLayout title={'TodoEdit'}>
+      {!!todo && (
+        <form className={styles.container} onSubmit={handleUpdateTodo}>
+          <div className={styles.area}>
+            <InputForm
+              value={inputTitle}
+              placeholder={'Title'}
+              onChange={handleChangeTitle}
+            />
+          </div>
+          <div className={styles.area}>
+            <TextArea
+              value={inputContent}
+              placeholder={'Content'}
+              onChange={handleChangeContent}
+            />
+          </div>
+          <div className={styles.area}>
+            <CommonButton type="submit" title="Edit Todo" />
+          </div>
+        </form>
+      )}
+    </BaseLayout>
+  );
+};
